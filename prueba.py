c9a48a4e36f8442c678bd7fcccb66d9ae90810cf
vharoc <vharoc@usal.es>
2024-05-06 15:57:26 +0200

    Load data 1

--- prueba.py
+++ prueba.py
@@ -1,2 +1,8 @@
 # primer commit
-print("Hola, mundo: py")
+import pandas as pd
+
+X = pd.read_csv('../ML_Diabetes/Diabetes-Data/data-01', sep='\t', header=None, names=['Fecha', 'Hora', 'Código', 'Valor'], dtype={'Fecha': str, 'Hora': str})
+X.set_index('Fecha', inplace=True)
+
+print(X.head())
+
