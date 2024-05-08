d1f003db5302598228a0ebcddb2c5e49bf35b77b
vharoc <vharoc@usal.es>
2024-05-08 19:00:37 +0200

    test data, train data, read me

--- prueba.py
+++ prueba.py
@@ -1,8 +1,13 @@
 # primer commit
 import pandas as pd
 
-X = pd.read_csv('../ML_Diabetes/Diabetes-Data/data-01', sep='\t', header=None, names=['Fecha', 'Hora', 'Código', 'Valor'], dtype={'Fecha': str, 'Hora': str})
-X.set_index('Fecha', inplace=True)
+X_train = pd.read_csv('../ML_Diabetes/Diabetes-Data/data-01', sep='\t', header=None, names=['Fecha', 'Hora', 'Código', 'Valor'], dtype={'Fecha': str, 'Hora': str})
+X_test = pd.read_csv('../ML_Diabetes/Diabetes-Data/data-02', sep='\t', header=None, names=['Fecha', 'Hora', 'Código', 'Valor'], dtype={'Fecha': str, 'Hora': str})
 
-print(X.head())
+X_train.set_index('Fecha', inplace=True)
+X_test.set_index('Fecha', inplace=True)
+
+print(X_train.head())
+print()
+print(X_test.head())
 
